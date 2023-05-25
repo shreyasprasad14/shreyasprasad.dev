b519f6fa6ef58ea87a1a9e48b835c03ee9fcfe8a
Shreyas Prasad <shreyasprasad14@gmail.com>
2023-05-25 03:44:44 -0400

    Scoring & End Screen

--- hosting/pages/anagrams.tsx
+++ hosting/pages/anagrams.tsx
@@ -4,8 +4,12 @@ import {ArrowLongRightIcon, ArrowPathRoundedSquareIcon} from "@heroicons/react/2
 import {useEffect, useState} from "react";
 import {Button} from "flowbite-react";
 import {Dialog, Transition} from "@headlessui/react";
+import {LocalToastTarget, useLocalToast} from "react-local-toast";
 
 const seedrandom = require('seedrandom');
+
+import 'react-local-toast/dist/bundle.css';
+
 
 enum GAME_STATE {
     INIT,
@@ -21,14 +25,24 @@ export async function getStaticProps() {
     }
 }
 export default function Anagrams({ words }) {
+
+    const { showToast } = useLocalToast();
+    const scoreTable = [0, 25, 50, 100, 400, 1200, 2000];
     const handleWordSubmit = () => {
         const word = userLetters.join('');
         if (!userWords.includes(word) && isPseudoAnagram(word, gameLetters) && words.includes(word)) {
-            console.log("Word: " + word);
+            const wordScore = scoreTable[word.length];
+            setScore(score + wordScore);
+            showToast("anagrams", `+${wordScore} points`, {type:"success", duration: 1000});
             setUserLetters([]);
+            setUserIndices([]);
             setUserWords([...userWords, word])
         } else {
-            console.log("Not a word: " + word);
+            if (userWords.includes(word)) {
+                showToast("anagrams", "Word already submitted", {type:"error", duration: 500});
+            } else {
+                showToast("anagrams", "Invalid word", {type: "error", duration: 500});
+            }
         }
     }
     const [gameLetters, setGameLetters] = useState<string[]>(generateLetterSet());
@@ -37,9 +51,11 @@ export default function Anagrams({ words }) {
 
     const [mutableLetters, setMutableLetters] = useState<string[]>(gameLetters);
     const [userLetters, setUserLetters] = useState<string[]>([]);
+    const [userIndices, setUserIndices] = useState<number[]>([]);
     const [userWords, setUserWords] = useState<string[]>([]);
 
-    const [scoreElements, setScoreElements] = useState<JSX.Element[]>([]);
+    const [score, setScore] = useState<number>(0);
+
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -59,11 +75,15 @@ export default function Anagrams({ words }) {
         function handleKeyDown(e) {
             if (e.key === 'Backspace') {
                 setUserLetters(userLetters.slice(0, userLetters.length - 1));
+                setUserIndices(userIndices.slice(0, userIndices.length - 1));
             }
             else if (e.key === 'Enter') {
                 handleWordSubmit();
             } else if (e.keyCode >= 65 && e.keyCode <= 90 && userLetters.length < 6) {
                 setUserLetters([...userLetters, e.key]);
+
+                const index = gameLetters.indexOf(e.key);
+                setUserIndices([...userIndices, index]);
             }
         }
 
@@ -88,10 +108,25 @@ export default function Anagrams({ words }) {
         );
     }
 
+    if (gameState === GAME_STATE.FINISHED) {
+        return (
+            <div>
+                <Navigation />
+                <EndDialog  score={score} words={userWords} />
+            </div>
+        );
+    }
+
     const clearLetter = (index: number) => {
         const newLetters = userLetters.slice();
+        const letterToRemove = newLetters[index];
         newLetters.splice(index, 1);
         setUserLetters(newLetters);
+
+        const newIndices = userIndices.slice();
+        const letterIndex = gameLetters.indexOf(letterToRemove);
+        newIndices.splice(newIndices.indexOf(letterIndex), 1);
+        setUserIndices(newIndices);
     }
 
     return (
@@ -101,60 +136,53 @@ export default function Anagrams({ words }) {
                 <h1 className="text-4xl text-center">Anagrams</h1>
                 <div className="flex justify-center items-center">
                     <div className="flex flex-col justify-center items-center">
-                        <div className="flex justify-center items-center">
-                            <Box letter={userLetters[0]} onClick={()=>clearLetter(0)} />
-                            <Box letter={userLetters[1]} onClick={()=>clearLetter(1)} />
-                            <Box letter={userLetters[2]} onClick={()=>clearLetter(2)} />
-                            <Box letter={userLetters[3]} onClick={()=>clearLetter(3)} />
-                            <Box letter={userLetters[4]} onClick={()=>clearLetter(4)} />
-                            <Box letter={userLetters[5]} onClick={()=>clearLetter(5)} />
-                            <ArrowLongRightIcon
-                                className="w-8 h-8 m-2 hover:cursor-pointer hover:text-blue-700"
-                                onClick={handleWordSubmit}
-                            />
-                        </div>
+                        <LocalToastTarget name={"anagrams"}>
+                            <div className="flex justify-center items-center">
+                                <Box letter={userLetters[0]} onClick={()=>clearLetter(0)} />
+                                <Box letter={userLetters[1]} onClick={()=>clearLetter(1)} />
+                                <Box letter={userLetters[2]} onClick={()=>clearLetter(2)} />
+                                <Box letter={userLetters[3]} onClick={()=>clearLetter(3)} />
+                                <Box letter={userLetters[4]} onClick={()=>clearLetter(4)} />
+                                <Box letter={userLetters[5]} onClick={()=>clearLetter(5)} />
+                                <ArrowLongRightIcon
+                                    className="w-8 h-8 m-2 hover:cursor-pointer hover:text-blue-700"
+                                    onClick={handleWordSubmit}
+                                />
+                            </div>
+                        </LocalToastTarget>
                     </div>
                 </div>
                 <div className="flex justify-center items-center hover:cursor-pointer">
-                    {gameLetters.map((letter,index) => {
+                    {mutableLetters.map((letter,index) => {
                         return (
                             <Box
                                 letter={letter}
                                 key={index}
                                 primaryTheme={true}
                                 onClick={() => {
-                                    if (userLetters.length < 6) {
+                                    if (userLetters.length < 6 && !userIndices.includes(index) && gameState === GAME_STATE.PLAYING) {
                                         setUserLetters([...userLetters, letter]);
+                                        setUserIndices([...userIndices, index]);
                                     }
                                 }}
+                                isUsed={userIndices.includes(index)}
                             />
                         )
                     })}
-                    <ArrowPathRoundedSquareIcon
-                        className="w-8 h-8 m-2 hover:cursor-pointer hover:text-blue-700"
-                        onClick={() => setGameLetters(shuffleLetters(gameLetters))}
-                    />
+                    {/*<ArrowPathRoundedSquareIcon*/}
+                    {/*    className="w-8 h-8 m-2 hover:cursor-pointer hover:text-blue-700"*/}
+                    {/*    onClick={() => setMutableLetters(shuffleLetters(mutableLetters))}*/}
+                    {/*/>*/}
                 </div>
                 <div className="flex justify-center items-center">
                     <h1 className="text-4xl">{countdown}</h1>
                 </div>
+                <div className="flex justify-center items-center">
+                    <h1 className="text-4xl text-blue-700">{score}</h1>
+                </div>
             </div>
         </div>
     )
-}
-
-function FloatAwayScore({ score }) {
-    // Test if this works
-    return (
-        <Transition>
-            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-                <div className="absolute top-0 left-0 w-full h-full bg-black/50" />
-                <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                    <h1 className="text-4xl text-white">{score}</h1>
-                </div>
-            </div>
-        </Transition>
-    );
 }
 
 function StartDialog({ setPlaying, state }) {
@@ -176,6 +204,37 @@ function StartDialog({ setPlaying, state }) {
     )
 }
 
+function EndDialog({ score, words }) {
+    const sortedWords = words.sort((a, b) => b.length - a.length);
+    return (
+        <>
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+            <div className="fixed inset-0 flex items-center justify-center p-4">
+                <Dialog open={true} onClose={() => {}} className="relative z-50 flex justify-center items-center">
+                    <Dialog.Panel className="w-full max-w-sm rounded bg-white">
+                        <Dialog.Title className="text-4xl text-center">Game Over!</Dialog.Title>
+                        <Dialog.Description className="text-center">Your score was: </Dialog.Description>
+                        <div className="flex justify-center items-center text-blue-700 text-3xl">
+                            {score}
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <div className="flex flex-col justify-center items-center">
+                                {sortedWords.map((word, index) => {
+                                    return (
+                                        <div key={index} className="flex justify-center items-center">
+                                            <div className="text-2xl text-center">{word}</div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </Dialog.Panel>
+                </Dialog>
+            </div>
+        </>
+    )
+}
+
 function shuffleLetters(letters: string[]): string[] {
     const newLetters: string[] = letters.slice();
     for (let i = 0; i < newLetters.length; i++) {
@@ -187,14 +246,15 @@ function shuffleLetters(letters: string[]): string[] {
     return newLetters;
 }
 
-function Box({ letter, primaryTheme=false, onClick=null}) {
+function Box({ letter, primaryTheme=false, onClick=null, isUsed=false }) {
     return (
         <div
             onClick={onClick}
             className={"w-16 h-16 m-2 flex justify-center items-center " +
+                (isUsed ? "bg-gray-400 hover:bg-gray-400 " : "") +
                 (primaryTheme ? "bg-blue-700 " : "bg-gray-200 ") +
                 (primaryTheme ? "hover:bg-blue-800 " : "hover:bg-gray-300 ") +
-                (letter === "" || letter === undefined ? "" : "hover:cursor-pointer")
+                (letter === "" || letter === undefined || isUsed ? "hover:cursor-not-allowed" : "hover:cursor-pointer")
         }>
             <h1 className={"text-4xl " + (primaryTheme ? "text-white" : "text-black")}>{letter}</h1>
         </div>
@@ -245,6 +305,11 @@ function generateLetterSet(): string[] {
         for (let j = 0; j < weights.length; j++) {
             sum += weights[j];
             if (randomNum <= sum) {
+               // If there are 2 or more of the same letter, only add it if there are less than 2 of it
+                if (letters.includes(allLetters[j]) && letters.filter((letter) => letter === allLetters[j]).length >= 2) {
+                    i--;
+                    break;
+                }
                 letters.push(allLetters[j]);
                 break;
             }
