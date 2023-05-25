bc596f6d1a3a1605c3ebbf0d0572f96f5269c06a
Shreyas Prasad <shreyasprasad14@gmail.com>
2023-05-25 04:22:47 -0400

    Revert "Merge pull request #1 from shreyasprasad14/test"
    This reverts commit ea4ed19663afef3a9f7a992f27618b8d3a83c685, reversing
    changes made to ec34cdb93a4a624dbecb664ce01651da946b73b6.

--- hosting/data/worddata.js
+++
@@ -1,8 +0,0 @@
-import fsPromises from 'fs/promises';
-import path from 'path';
-
-export async function getWordData() {
-    const filePath = path.join(process.cwd(), 'data/words.txt');
-    const fileData = await fsPromises.readFile(filePath, 'utf8');
-    return fileData.split('\r\n');
-}
--- hosting/pages/_app.tsx
+++ hosting/pages/_app.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head';
-import {LocalToastProvider} from "react-local-toast";
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <LocalToastProvider>
-            <Head>
-                <link rel="icon" href="/favicon.svg" />
-                <title>Shreyas Prasad</title>
-            </Head>
-            <Component {...pageProps} />
-        </LocalToastProvider>
+        <>
+        <Head>
+            <link rel="icon" href="/favicon.svg" />
+            <title>Shreyas Prasad</title>
+        </Head>
+        <Component {...pageProps} />
+        </>
     );
 }
