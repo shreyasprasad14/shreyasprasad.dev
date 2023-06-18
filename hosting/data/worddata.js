17cd83b97bdcce05c195989e230ebcbc69622c81
Shreyas Prasad <shreyasprasad14@gmail.com>
2023-06-18 01:17:26 -0400

    Update worddata.js

--- hosting/data/worddata.js
+++ hosting/data/worddata.js
@@ -2,7 +2,7 @@ import fsPromises from 'fs/promises';
 import path from 'path';
 
 export async function getWordData() {
-    const filePath = path.join(process.cwd(), './data/words.txt');
+    const filePath = path.join(process.cwd(), './hosting/data/words.txt');
     const fileData = await fsPromises.readFile(filePath, 'utf8');
     return fileData.split('\n');
 }
